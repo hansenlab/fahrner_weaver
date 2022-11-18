e6358fbe997da494cd36dd7d45a5c5f31b7869c3
Greider Lab <greiderlab@bsno12236.acasmart.jh.edu>
2022-11-18 15:44:30 -0500

    GSE138980 and EZH2 target analyses

---
+++ RNAseq/EZH2_targets/RNAseq_analysis-EZH2.R
@@ -0,0 +1,85 @@
+library(rtracklayer)
+library(EnsDb.Hsapiens.v75)
+library(BSgenome.Hsapiens.UCSC.hg19)
+library(biomaRt)
+library(DESeq2)
+
+# session <- browserSession('UCSC')
+# genome(session) <- 'hg19'
+# # query <- ucscTableQuery(session, 'Txn Factor ChIP', )
+# # tableName(query) <- 'wgEncodeRegTfbsClusteredV3'
+# query <- ucscTableQuery(session, table='wgEncodeRegTfbsClusteredV3')
+# tf_table_query <- getTable(query)
+# tf_table_query_EZH2 <- tf_table_query[which(tf_table_query$name=='EZH2'),]
+
+# get GRanges of human EZH2 binding peaks
+tf_table <- read.csv(file='tf_table.csv')
+tf_table_EZH2 <- tf_table[which(tf_table$name=='EZH2'),]
+EZH2_h19 <- tf_table_EZH2[,c('chrom','chromStart','chromEnd','name','sourceCount')]
+EZH2_h19_granges <- makeGRangesFromDataFrame(EZH2_h19, keep.extra.columns=TRUE)
+genome(EZH2_h19_granges) <- 'hg19'
+save(EZH2_h19_granges, file='EZH2_h19_granges.rda')
+
+# separate GRanges by level of evidence
+load(file='EZH2_h19_granges.rda')
+EZH2_all <- EZH2_h19_granges
+EZH2_mod <- EZH2_h19_granges[which(EZH2_h19_granges$sourceCount >= 2)]
+EZH2_strong <- EZH2_h19_granges[which(EZH2_h19_granges$sourceCount >= 5)]
+
+# get GRanges of all human promoters
+edb <- EnsDb.Hsapiens.v75
+proms_all <- promoters(edb, upstream=2000, downstream=2000,
+                       columns = c("gene_name", "tx_id", "tx_cds_seq_start", "tx_cds_seq_end",
+                                   "tx_biotype", "gene_id"))
+genome(seqinfo(proms_all)) <- 'hg19'
+seqlevelsStyle(proms_all) <- 'ucsc'
+
+# overlap human EZH2 binding peaks with human promoters (human EZH2 targets)
+proms_all <- proms_all[which(seqnames(proms_all) %in% names(Hsapiens)[1:24])]
+EZH2_target_id_all <- unique(proms_all$gene_id[queryHits(findOverlaps(proms_all, EZH2_all))])
+EZH2_target_id_mod <- unique(proms_all$gene_id[queryHits(findOverlaps(proms_all, EZH2_mod))])
+EZH2_target_id_strong <- unique(proms_all$gene_id[queryHits(findOverlaps(proms_all, EZH2_strong))])
+
+# find mouse homologs of human EZH2 targets
+human <- useMart('ensembl', dataset='hsapiens_gene_ensembl')
+searchAttributes(human, pattern='mmusculus')
+human_mouse_homologs <- getBM(attributes = c("ensembl_gene_id", "external_gene_name",
+                                             "mmusculus_homolog_ensembl_gene", 
+                                             "mmusculus_homolog_associated_gene_name", 
+                                             "mmusculus_homolog_orthology_confidence", 
+                                             "mmusculus_homolog_perc_id_r1"),
+                              filters = "ensembl_gene_id",
+                              values = EZH2_target_id_all,
+                              mart = human)
+human_mouse_homologs <- human_mouse_homologs[
+  which(human_mouse_homologs$mmusculus_homolog_orthology_confidence==1),]
+mouse_EZH2_target_all <- unique(human_mouse_homologs$mmusculus_homolog_ensembl_gene)
+save(mouse_EZH2_target_all, file='mouse_EZH2_target_all.rda')
+
+human_mouse_homologs_strong <- getBM(attributes = c("ensembl_gene_id", "external_gene_name",
+                                             "mmusculus_homolog_ensembl_gene", 
+                                             "mmusculus_homolog_associated_gene_name", 
+                                             "mmusculus_homolog_orthology_confidence", 
+                                             "mmusculus_homolog_perc_id_r1"),
+                              filters = "ensembl_gene_id",
+                              values = EZH2_target_id_strong,
+                              mart = human)
+human_mouse_homologs_strong <- human_mouse_homologs_strong[
+  which(human_mouse_homologs_strong$mmusculus_homolog_orthology_confidence==1),]
+mouse_EZH2_target_strong <- unique(human_mouse_homologs_strong$mmusculus_homolog_ensembl_gene)
+save(mouse_EZH2_target_strong, file='mouse_EZH2_target_strong.rda')
+
+# p-value histograms of EZH2 targets in final_analysis data
+setwd("~/Desktop/temp/Fahrner lab/Data/Weaver/fahrner_weaver/RNAseq/final_analysis/BigStuff")
+load(file='dds_final.rda')
+res <- results(dds)
+hist(res$pvalue[which(rownames(res) %in% mouse_EZH2_target_all)], 
+     freq=FALSE, breaks=50,
+     xlab='p-values (R684C/+ vs WT)',
+     main='EZH2 target genes',
+     ylim=c(0,4.5))
+hist(res$pvalue[!(rownames(res) %in% mouse_EZH2_target_all)], 
+     freq=FALSE, breaks=50,
+     xlab='p-values (R684C/+ vs WT)',
+     main='non-EZH2 target genes',
+     ylim=c(0,4.5))
---
+++ RNAseq/GSE138980/RNAseq_GSE138980.R
@@ -0,0 +1,130 @@
+library(TxDb.Mmusculus.UCSC.mm10.ensGene)
+library(tximport)
+library(DESeq2)
+library(sva)
+library(biomaRt)
+library(ggplot2)
+library(ggrepel)
+library(tximeta)
+library(SummarizedExperiment)
+library(edgeR)
+library(limma)
+library(statmod)
+library(tidyverse)
+
+
+### BEGIN IMPORT
+samples <- read.csv('RNAseq-samples.csv')
+
+dir <- '~/Desktop/temp/Fahrner lab/Data/Weaver/fahrner_weaver/RNAseq/GSE138980/quants_final'
+files <- file.path(dir, paste(samples$Sample.ID, '_quant', sep=''), 'quant.sf')
+
+file.exists(files)
+coldata <- data.frame(files, names=samples$Sample.ID, condition=samples$Genotype, stringsAsFactors = FALSE)
+
+# import transcript abundances
+se <- tximeta(coldata)
+
+# summarize abundances to gene level
+gse <- summarizeToGene(se)
+
+# load SummarizedExperiment into DESeqDataSet
+dds <- DESeqDataSet(gse, design=~condition)
+### END IMPORT
+
+
+### BEGIN ANALYSIS 
+#dds$condition <- relevel(dds$condition, ref="Ctrl")
+plot <- plotPCA(vst(dds))
+setEPS()
+postscript('GSE138980_PCA-uncollapsed.eps')
+plot + geom_label_repel(aes(label=gsub("-.*", "", colnames(dds))), show.legend = FALSE)
+dev.off()
+
+# # collapse technical replicates
+# dds$sample <- factor(gsub("-.*", "", colnames(dds)))
+# dds.coll <- collapseReplicates(dds, dds$sample)
+# stopifnot(all(rowSums(counts(dds[,which(dds$sample=="705")])) == counts(dds.coll[,1])))
+# dds.coll.counts <- counts(dds.coll)
+
+# # filter genes with low median counts
+# keep <- rowMedians(counts(dds.coll)) > 10
+# dds.coll.filtered <- dds.coll[keep,]
+keep <- rowMedians(counts(dds)) > 10
+dds.filtered <- dds[keep,]
+
+plot <- plotPCA(vst(dds.filtered))
+
+setEPS()
+postscript('GSE138980_PCA-filtered.eps')
+plot + geom_label_repel(aes(label=gsub("-.*", "", colnames(dds.filtered))), show.legend = FALSE)
+dev.off()
+
+# # find surrogate variables for batch effects using sva
+# dds.coll.filtered$condition <- relevel(dds.coll.filtered$condition, ref="WT")
+# dds.coll.filtered.counts <- counts(dds.coll.filtered)
+# mod <- model.matrix(~condition, colData(dds.coll.filtered))
+# mod0 <- model.matrix(~1, colData(dds.coll.filtered))
+# svseq <- svaseq(dds.coll.filtered.counts, mod, mod0)
+dds.filtered$condition <- relevel(dds.filtered$condition, ref="Ctrl")
+dds.filtered.counts <- counts(dds.filtered)
+mod <- model.matrix(~condition, colData(dds.filtered))
+mod0 <- model.matrix(~1, colData(dds.filtered))
+svseq <- svaseq(dds.filtered.counts, mod, mod0)
+
+# append surrogate variables
+ddssva <- dds.filtered
+ddssva$SV1 <- svseq$sv[,1]
+ddssva$SV2 <- svseq$sv[,2]
+# ddssva$SV3 <- svseq$sv[,3]
+# design(ddssva) <- formula(~SV1 + SV2 + SV3 + condition)
+design(ddssva) <- formula(~SV1 + SV2 + condition)
+
+# DESeq2 call
+ddssva <- DESeq(ddssva)
+dds <- ddssva
+save(dds, file='dds_GSE138980.rda')
+res <- results(dds)
+
+diffexp.subset <- as.data.frame(res[which(res$padj <0.1),])
+### END ANALYSIS
+
+# gene annotation
+ensembl.id <- rownames(diffexp.subset)
+
+ensembl102.mmusculus <- useEnsembl(biomart='genes', dataset='mmusculus_gene_ensembl', version=102)
+mgi <- getBM(attributes=c('ensembl_gene_id','mgi_symbol','mgi_description'),
+             filters='ensembl_gene_id',
+             values=ensembl.id,
+             mart=ensembl102.mmusculus)
+diffexp.subset$ensembl_gene_id <- ensembl.id
+diffexp.subset <- merge(diffexp.subset, mgi, by='ensembl_gene_id')
+diffexp.subset <- diffexp.subset[order(diffexp.subset$log2FoldChange, decreasing=TRUE),]
+write.csv(diffexp.subset, file="diffexp_GSE138980.csv")
+
+hist(res$pvalue, freq=FALSE, breaks=50, 
+     main='GSK126 vs Ctrl',
+     xlab='p-values',
+     ylim=c(0,8))
+
+setwd("~/Desktop/temp/Fahrner lab/Data/Weaver/fahrner_weaver/RNAseq/final_analysis/BigStuff")
+load(file='dds_final.rda')
+dds_final <- dds
+setwd("~/Desktop/temp/Fahrner lab/Data/Weaver/fahrner_weaver/RNAseq/GSE138980")
+load(file='dds_GSE138980.rda')
+dds_GSE138980 <- dds
+rm(dds)
+res_final <- results(dds_final)
+res_GSE138980 <- results(dds_GSE138980)
+gene_ids_final_less <- rownames(res_final[which(res_final$padj<0.1),])
+gene_ids_final_more <- rownames(res_final[which(res_final$padj>0.1),])
+hist(res_GSE138980[which(rownames(res_GSE138980) %in% gene_ids_final_less),]$pvalue,
+     freq=FALSE, breaks=50,
+     xlab='p-values (GSK126 vs Ctrl)',
+     main='FDR < 0.1, R684C/+ vs WT',
+     ylim=c(0,12))
+hist(res_GSE138980[which(rownames(res_GSE138980) %in% gene_ids_final_more),]$pvalue,
+     freq=FALSE, breaks=50,
+     xlab='p-values (GSK126 vs Ctrl)',
+     main='FDR > 0.1, R684C/+ vs WT',
+     ylim=c(0,12))
